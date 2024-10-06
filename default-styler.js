588ddbe434ac82cdef38cc4e72b5bc40ffb0768a
nousantx <nousantx@gmail.com>
2024-10-06 09:36:29 +0700

    feat: move default-styler.js => tenoxui/core-styler-default.js

--- default-styler.js
+++
@@ -1,8 +0,0 @@
-import { makeTenoxUI } from '@tenoxui/core'
-import { property } from '@tenoxui/property'
-
-export function styler() {
-  document.querySelectorAll('*[class]').forEach(element => {
-    new makeTenoxUI({ element, property }).useDOM()
-  })
-}
