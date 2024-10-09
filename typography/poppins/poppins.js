5958019aa60b4b73e46c47b55e8fcb4a8adb4d0c
nousantx <nousantx@gmail.com>
2024-10-09 18:00:08 +0700

    feat(typography): add poppins type scales

---
+++ typography/poppins/poppins.js
@@ -0,0 +1,15 @@
+const tenoxuiType = {
+  fs: 'fontSize',
+  fw: 'fontwight',
+  lh: 'lineHeight',
+  ls: 'letterSpacing'
+}
+
+const tenoxuiClass = {
+  h1: 'fs-2.5rem fw-600 lh-1.15 ls--0.04em',
+  h2: 'fs-2rem fw-500 lh-1.25 ls--0.035em',
+  h3: 'fs-1.75rem fw-500 lh-1.3 ls--0.035em',
+  h4: 'fs-1.5rem fw-500 lh-1.35 ls--0.03em',
+  p: 'fs-1rem fw-500 lh-1.25 ls--0.038em',
+  small: 'fs-12px fw-500 lh-1.3 ls--0.02em'
+}
